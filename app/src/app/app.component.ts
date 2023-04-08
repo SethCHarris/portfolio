f07768b71c0d12c0610e6a7a1fb8dd4645f8bfb4
Seth Harris <schristianharris@gmail.com>
2023-04-08 00:35:28 -0500

    Basic text transition in header text.

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'app';
+  public headerOptions: string[] = [
+    'problem solver',
+    'computer scientist',
+    'tech enthusiast',
+    'programmer',
+  ];
+  public currentHeaderIndex: number = 0;
+  public currentHeaderOption: string =
+    this.headerOptions[this.currentHeaderIndex];
+
+  ngOnInit() {
+    console.log(this.headerOptions[0]);
+    this.updateHeaderOption();
+    setInterval(() => {
+      this.updateHeaderOption();
+    }, 2500);
+  }
+
+  updateHeaderOption() {
+    this.currentHeaderIndex = (this.currentHeaderIndex + 1) % 4;
+    this.currentHeaderOption = this.headerOptions[this.currentHeaderIndex];
+  }
 }
