5c76dc57ed7e578a09c64c76fd01c0b022a3abf0
Seth Harris <schristianharris@gmail.com>
2023-04-10 11:48:15 -0500

    Generating random viewer count, added ordinal generator function to visitor count.

--- app/src/app/thanks/thanks.component.ts
+++ app/src/app/thanks/thanks.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-thanks',
   templateUrl: './thanks.component.html',
-  styleUrls: ['./thanks.component.scss']
+  styleUrls: ['./thanks.component.scss'],
 })
 export class ThanksComponent {
+  public totalVisitors: number = 0;
+  public totalVisitorsWithSuffix: string = '';
 
+  ngOnInit() {
+    this.totalVisitors = Math.floor(Math.random() * 10);
+    this.totalVisitorsWithSuffix = this.getSuffixOfNumber(this.totalVisitors);
+  }
+
+  getSuffixOfNumber(num: number) {
+    var j = num % 10,
+      k = num % 100;
+    if (j == 1 && k != 11) {
+      return num + 'st';
+    }
+    if (j == 2 && k != 12) {
+      return num + 'nd';
+    }
+    if (j == 3 && k != 13) {
+      return num + 'rd';
+    }
+    return num + 'th';
+  }
 }
