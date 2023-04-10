3714a66caf59ac1d18122904b1ebcc9b89e808f5
Seth Harris <schristianharris@gmail.com>
2023-04-09 19:43:36 -0500

    Added assets. Portrait header done.

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -3,29 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public headerOptions: string[] = [
-    'problem solver',
-    'computer scientist',
-    'tech enthusiast',
-    'programmer',
-  ];
-  public currentHeaderIndex: number = 0;
-  public currentHeaderOption: string =
-    this.headerOptions[this.currentHeaderIndex];
-
-  ngOnInit() {
-    console.log(this.headerOptions[0]);
-    this.updateHeaderOption();
-    setInterval(() => {
-      this.updateHeaderOption();
-    }, 2500);
-  }
-
-  updateHeaderOption() {
-    this.currentHeaderIndex = (this.currentHeaderIndex + 1) % 4;
-    this.currentHeaderOption = this.headerOptions[this.currentHeaderIndex];
-  }
+  title = 'app';
 }
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PictureComponent } from './picture/picture.component';
+import { IntroComponent } from './intro/intro.component';
+import { LanguagesComponent } from './languages/languages.component';
+import { CertsComponent } from './certs/certs.component';
+import { GithubComponent } from './github/github.component';
+import { ThanksComponent } from './thanks/thanks.component';
+import { SnakeComponent } from './snake/snake.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PictureComponent,
+    IntroComponent,
+    LanguagesComponent,
+    CertsComponent,
+    GithubComponent,
+    ThanksComponent,
+    SnakeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
