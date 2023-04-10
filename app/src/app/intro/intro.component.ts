9ef150d7b82384de11484a3f2f24926a06bd1ea6
Seth Harris <schristianharris@gmail.com>
2023-04-09 21:39:43 -0500

    Finished typewriter effect for intro text.

--- app/src/app/intro/intro.component.ts
+++ app/src/app/intro/intro.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
-  styleUrls: ['./intro.component.scss']
+  styleUrls: ['./intro.component.scss'],
 })
 export class IntroComponent {
+  public currentIntroTextIndex: number = 0;
 
+  ngOnInit() {
+    setInterval(() => {
+      this.updateIntroTextIndex();
+    }, 5000);
+  }
+
+  updateIntroTextIndex() {
+    this.currentIntroTextIndex = (this.currentIntroTextIndex + 1) % 4;
+    console.log(this.currentIntroTextIndex);
+    console.log('running');
+  }
 }
