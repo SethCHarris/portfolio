acc7a7ca1d7703ac4ae401fe1f6d4e5997156a22
Seth Harris <schristianharris@gmail.com>
2023-04-10 21:24:03 -0500

    Started work on snake game.

--- app/src/app/snake/snake.component.ts
+++ app/src/app/snake/snake.component.ts
@@ -3,8 +3,41 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-snake',
   templateUrl: './snake.component.html',
-  styleUrls: ['./snake.component.scss']
+  styleUrls: ['./snake.component.scss'],
 })
 export class SnakeComponent {
+  public gameStarted: boolean = false;
+  public currentSnakeDirection: string = 'UP';
+  public boardHeight: number[] = [];
 
+  ngOnInit() {
+    this.boardHeight = [...Array(10).keys()];
+    document.onkeydown = (e) => {
+      switch (e.key) {
+        case 'a':
+          this.directionKeyPressed('LEFT');
+          break;
+        case 'w':
+          this.directionKeyPressed('UP');
+          break;
+        case 's':
+          this.directionKeyPressed('DOWN');
+          break;
+        case 'd':
+          this.directionKeyPressed('RIGHT');
+          break;
+      }
+    };
+  }
+
+  directionKeyPressed(direction: string) {
+    if (this.gameStarted) {
+      this.currentSnakeDirection = direction;
+      console.log(this.currentSnakeDirection);
+    }
+  }
+
+  startGame() {
+    this.gameStarted = true;
+  }
 }
--- app/src/app/thanks/thanks.component.ts
+++ app/src/app/thanks/thanks.component.ts
@@ -10,7 +10,7 @@ export class ThanksComponent {
   public totalVisitorsWithSuffix: string = '';
 
   ngOnInit() {
-    this.totalVisitors = Math.floor(Math.random() * 10);
+    this.totalVisitors = Math.floor(Math.random() * 1000);
     this.totalVisitorsWithSuffix = this.getSuffixOfNumber(this.totalVisitors);
   }
 
